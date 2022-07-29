7796d78e110b874e5de8839261ecb320ba5c2f8b
Federico <info@fdossena.com>
2022-07-29 16:34:06 +0200

    Moved archive feature out of run.sh

--- template-base/run.sh
+++ template-base/run.sh
@@ -9,25 +9,7 @@ done
 DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 cd "$DIR"
 if [ "$1" == "archive" ]; then
-    cd ..
-    folderPath="$(basename "$DIR")"
-    archivePath="$(realpath "$folderPath")"
-    if [ "$2" == "nocompress" ]; then
-        archivePath="$archivePath.tar"
-        echo "Archiving to $archivePath without compression"
-        sleep 1
-        tar -cvf "$archivePath" "$folderPath"
-    else
-        archivePath="$archivePath.tar.zst"
-        echo "Compressing to $archivePath"
-        sleep 1
-        tar -I 'zstd --ultra -22' -cvf "$archivePath" "$folderPath"
-    fi
-    if [ $? -ne 0 ]; then
-        echo "Failed"
-    else
-        echo "Saved to $archivePath"
-    fi
+    ./system/archive.sh $*
     exit
 fi
 USE_STEAMRT=1
---
+++ template-base/system/archive.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+folderPath="$(basename "$(pwd)")"
+cd ..
+archivePath="$(realpath "$folderPath")"
+if [ "$2" == "nocompress" ]; then
+    archivePath="$archivePath.tar"
+    echo "Archiving to $archivePath without compression"
+    sleep 1
+    tar -cvf "$archivePath" "$folderPath"
+else
+    archivePath="$archivePath.tar.zst"
+    echo "Compressing to $archivePath"
+    sleep 1
+    tar -I 'zstd --ultra -22' -cvf "$archivePath" "$folderPath"
+fi
+if [ $? -ne 0 ]; then
+    echo "Failed"
+else
+    echo "Saved to $archivePath"
+fi
