3f2f58965c7ae2456d7f6ecba12d2045694a59d5
Federico Dossena <info@fdossena.com>
2023-07-17 15:28:13 +0200

    Changed wine build config

--- wine/build.sh
+++ wine/build.sh
@@ -5,14 +5,15 @@ rm -rf build repo
 mkdir build
 mkdir repo
 cd repo
-git clone --recursive https://github.com/Frogging-Family/wine-tkg-git
+git clone https://github.com/Frogging-Family/wine-tkg-git
 cd wine-tkg-git/wine-tkg-git
+#tkg is supposed to have an _EXT_CONFIG_PATH variable but it doesn't work (or if it does it doesn't disable the stupid prompt at the beginning) so I just copy the config into it, fuck it
 cp ../../../wine-tkg-tdf-games.cfg wine-tkg-profiles
 cp ../../../wine-tkg-tdf-mainline.cfg wine-tkg-profiles
-echo "_LOCAL_PRESET=\"tdf-games\"" >> customization.cfg
+echo "_LOCAL_PRESET=\"tdf-games\"" > customization.cfg
 ./non-makepkg-build.sh
 mv non-makepkg-builds/wine-tkg-tdf-games-* ../../../build/wine-games
-echo "_LOCAL_PRESET=\"tdf-mainline\"" >> customization.cfg
+echo "_LOCAL_PRESET=\"tdf-mainline\"" > customization.cfg
 ./non-makepkg-build.sh
 mv non-makepkg-builds/wine-tkg-tdf-mainline-* ../../../build/wine-mainline
 cd ../../../build/wine-games
