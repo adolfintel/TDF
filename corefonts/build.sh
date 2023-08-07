874f5da0bae7267b8ebeef05c39c82ca5a4aaaac
Federico Dossena <info@fdossena.com>
2023-08-07 14:35:58 +0200

    Minor improvement to reliability of corefonts build script

--- corefonts/build.sh
+++ corefonts/build.sh
@@ -4,12 +4,19 @@ rm -rf build temp
 mkdir build temp
 cd temp
 exes=("andale32" "arial32" "arialb32" "comic32" "courie32" "georgi32" "impact32" "times32" "trebuc32" "verdan32" "webdin32")
+set +e
 for exe in ${exes[@]}; do
     for i in {1..20}; do #sometimes a sourceforge mirror is down and if we do this it will try a different one
         wget --tries=1 "https://sourceforge.net/projects/corefonts/files/the fonts/final/$exe.exe"
-        if [ $? -eq 0 ]; then break; fi;
+        if [ $? -eq 0 ]; then
+            break;
+        fi
     done
+    if [ ! -f "$exe.exe" ]; then
+        exit 1
+    fi
 done
+set -e
 cabextract *.exe
 mv *.TTF *.ttf ../build
 cd ..
