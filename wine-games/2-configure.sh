cba7293a33a6d9411dde3c9b21f3c530953273a1
Federico Dossena <info@fdossena.com>
2023-09-22 08:27:33 +0200

    Renamed config file for wine-games to workaround weird tkg implementation

--- wine-games/2-configure.sh
+++ wine-games/2-configure.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 echo 2 > state
-cp wine-tkg-tdf-games.cfg wine-tkg-git/wine-tkg-git/wine-tkg-profiles/
-echo "_LOCAL_PRESET=\"tdf-games\"" >> wine-tkg-git/wine-tkg-git/customization.cfg
+cp wine-tkg-valve-exp-bleeding-tdf.cfg wine-tkg-git/wine-tkg-git/wine-tkg-profiles/
+echo "_LOCAL_PRESET=\"valve-exp-bleeding-tdf\"" >> wine-tkg-git/wine-tkg-git/customization.cfg
 echo 3 > state
 exit 0
