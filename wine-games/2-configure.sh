7cb08bcbd26b766522814591a8a7c71a3d4342cc
Federico Dossena <info@fdossena.com>
2024-03-08 07:08:45 +0100

    Temporarily added a workaround to force the wine username to be set to "wine"

--- wine-games/2-configure.sh
+++ wine-games/2-configure.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 echo 2 > state
+rm -rf wine-tkg-git/wine-tkg-git/wine-tkg-userpatches/*
+cp 0001-Hardcode-username-to-wine.mylatepatch wine-tkg-git/wine-tkg-git/wine-tkg-userpatches
 cat wine-tkg-valve-exp-bleeding-tdf.cfg >> wine-tkg-git/wine-tkg-git/wine-tkg-profiles/wine-tkg-valve-exp-bleeding.cfg
 echo "_LOCAL_PRESET=\"valve-exp-bleeding\"" >> wine-tkg-git/wine-tkg-git/customization.cfg
 echo 3 > state
--- wine-mainline/2-configure.sh
+++ wine-mainline/2-configure.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 echo 2 > state
+rm -rf wine-tkg-git/wine-tkg-git/wine-tkg-userpatches/*
+cp 0001-Hardcode-username-to-wine.mylatepatch wine-tkg-git/wine-tkg-git/wine-tkg-userpatches
 echo "_LOCAL_PRESET=\"mainline\"" >> wine-tkg-git/wine-tkg-git/customization.cfg
 cat wine-tkg-mainline-tdf.cfg >> wine-tkg-git/wine-tkg-git/wine-tkg-profiles/wine-tkg-mainline.cfg
 echo 3 > state
