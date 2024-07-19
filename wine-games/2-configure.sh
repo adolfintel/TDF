ef52723163eaef0099db1468ed2e28d92208c5ee
Federico Dossena <info@fdossena.com>
2024-07-19 19:39:47 +0200

    Added temporary workaround for the gstreamer mess on bleeding valve wine

--- wine-games/2-configure.sh
+++ wine-games/2-configure.sh
@@ -4,6 +4,10 @@ echo 2 > state
 rm -rf wine-tkg-git/wine-tkg-git/wine-tkg-userpatches/*
 cp patches/* wine-tkg-git/wine-tkg-git/wine-tkg-userpatches
 cat wine-tkg-valve-exp-bleeding-tdf.cfg >> wine-tkg-git/wine-tkg-git/wine-tkg-profiles/wine-tkg-valve-exp-bleeding.cfg
-echo "_LOCAL_PRESET=\"valve-exp-bleeding\"" >> wine-tkg-git/wine-tkg-git/customization.cfg
+#echo "_LOCAL_PRESET=\"valve-exp-bleeding\"" >> wine-tkg-git/wine-tkg-git/customization.cfg
+#temporary workaround until the gstreamer mess in bleeding is fixed
+\cp wine-tkg-git/wine-tkg-git/wine-tkg-profiles/wine-tkg-valve-exp-bleeding.cfg wine-tkg-git/wine-tkg-git/wine-tkg-profiles/wine-tkg-valve-exp.cfg
+echo "_LOCAL_PRESET=\"valve-exp\"" >> wine-tkg-git/wine-tkg-git/customization.cfg
+#end workaround
 echo 3 > state
 exit 0
