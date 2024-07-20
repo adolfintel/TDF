5906111301898af13867acd4dba32d7822833e84
Federico Dossena <info@fdossena.com>
2024-07-20 09:30:23 +0200

    Added workaround for randomly failed downloads of vcredist

--- vcredist/1-fetch.sh
+++ vcredist/1-fetch.sh
@@ -10,8 +10,26 @@ if [ ! -d build ]; then
     mkdir build
 fi
 cd build
-wget -N https://aka.ms/vs/17/release/vc_redist.x64.exe
-wget -N https://aka.ms/vs/17/release/vc_redist.x86.exe
+while true; do
+    wget --referer "https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170" -N https://aka.ms/vs/17/release/vc_redist.x64.exe
+    if ! file vc_redist.x64.exe | grep "Windows" > /dev/null; then
+        echo "Looks like we got served garbage, retrying"
+        rm -f vc_redist.x64.exe
+        sleep 3
+    else
+        break
+    fi
+done
+while true; do
+    wget --referer "https://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170" -N https://aka.ms/vs/17/release/vc_redist.x86.exe
+    if ! file vc_redist.x86.exe | grep "Windows" > /dev/null; then
+        echo "Looks like we got served garbage, retrying"
+        rm -f vc_redist.x86.exe
+        sleep 3
+    else
+        break
+    fi
+done
 cd ..
 rm -f incomplete
 echo 2 > state
