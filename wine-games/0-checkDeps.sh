c63f285959a89426c2eb9a44ea5fd0722152b6b4
Federico Dossena <info@fdossena.com>
2025-04-18 12:09:40 +0200

    Added more dependency checks before building wine

--- wine-games/0-checkDeps.sh
+++ wine-games/0-checkDeps.sh
@@ -4,5 +4,21 @@ if ! hasCommand git; then
     echo "Git not installed"
     failed=1
 fi
+if ! hasCommand automake; then
+    echo "automake not installed"
+    failed=1
+fi
+if ! hasCommand flex; then
+    echo "flex not installed"
+    failed=1
+fi
+if ! hasCommand bison; then
+    echo "bison not installed"
+    failed=1
+fi
+if ! hasCommand pkg-config; then
+    echo "pkgconf not installed"
+    failed=1
+fi
 #wine-tkg's build script has its own dependency checks
 exit $failed
