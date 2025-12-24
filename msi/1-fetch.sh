d16233bca5ab4b814221cfd696fc2529730d950e
Federico Dossena <info@fdossena.com>
2025-12-24 19:24:51 +0100

    Updated winemono to 10.4.1

--- msi/1-fetch.sh
+++ msi/1-fetch.sh
@@ -10,7 +10,7 @@ if [ ! -d build ]; then
 fi
 cd build
 echo "Downloading winemono"
-ver='10.4.0'
+ver='10.4.1'
 mustDownload=0
 if [ -f ../winemono_version ]; then
     if [ "$(cat ../winemono_version)" != "$ver" ]; then
