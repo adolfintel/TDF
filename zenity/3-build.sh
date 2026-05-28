127870f82af4c44bdea2563ada76a8842bc7f95b
Federico Dossena <info@fdossena.com>
2026-05-28 19:17:12 +0200

    Updated zenity build script

--- zenity/3-build.sh
+++ zenity/3-build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 echo 3 > state
-echo "Building zenity (3.44)"
+echo "Building zenity (4.2)"
 rm -rf build
 mkdir build
 cd zenity
 meson ../build
-sed -i "s/#define ZENITY_DATADIR*/#define ZENITY_DATADIR \".\/system\/zenity\/data\"\/\//g" ../build/config.h
+sed -i 's/#define PACKAGE_DATADIR.*/#define PACKAGE_DATADIR ".\/system\/zenity\/data"/' ../build/config.h
 meson compile -C ../build
 cd ..
 mkdir build2
